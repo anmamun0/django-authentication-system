09565ad0489de2384372d305237f41a6bf2b4c8c
anmamun0 <anmamun0@gmail.com>
2025-11-02 19:07:36 +0600

    feat(users): add User API with public and authenticated `/auth/users/`, `/auth/users/me/` endpoints
    Created UserView using ModelViewSet with UserSerializers (excluding password). All /users/ endpoints are publicly accessible (AllowAny), while /users/me/ endpoint requires authentication (IsAuthenticated) to return the currently authenticated user's details. Registered viewset with DefaultRouter.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render 
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import UserSerializers
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+# Create your views here.
+
+class UserView(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    permission_classes = [AllowAny]
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        """
+        #  action:  GET /me/
+        # details: Returns the currently authenticated user.
+        """
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
